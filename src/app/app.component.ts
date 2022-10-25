31106d58a5cbe1fed7c88458538df971e2fad500
Sophie Cheng <chengsophie25@gmail.com>
2022-10-25 14:48:18 +0200

    data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit{
+  // test
+  weatherData!: WeatherData;
+  temp!: number;
+  humidity!: number;
+  wind_speed!: number;
+  cityName!: string;
+
+  constructor(private weatherService: WeatherService){
+
+  }
+
+  // weatherData?: WeatherData;
+
+  ngOnInit(): void {
+      this.weatherService.getWeatherData('Paris')
+      .subscribe({
+        next: (response) => {
+          this.weatherData = response;
+
+          this.temp = (this.weatherData.main.temp);
+          this.humidity = this.weatherData.main.humidity;
+          this.wind_speed = this.weatherData.wind.speed;
+          this.cityName = this.weatherData.name;
+
+          console.log(response);
+
+          console.log(this.temp);
+          console.log(this.humidity);
+          console.log(this.wind_speed);
+          console.log(this.cityName);
+        }
+      })
+  }
 }
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { WeatherData } from '../models/weather.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    const path = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=5ad210e7135ed0d49bdca1e1d5257daf`;
+    return this.http.get<WeatherData>(path, {
+      params: new HttpParams()
+      .set('q', cityName)
+      .set('units', 'metric')
+      .set('mode', 'json')
+      
+    })
+  }
+}
